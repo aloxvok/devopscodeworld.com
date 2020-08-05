abf4815792b567a6e31f8770e7b74605a025e1a5
Hector Pinot <hpinot9@gmail.com>
2020-08-04 17:37:51 -0700

    Update application.py

--- application.py
+++ application.py
@@ -31,6 +31,8 @@ instructions = '''
     <hr/>
     <h1 style="background-color: aqua; color: red">You can see the tiny houses!</h1>
     <hr/>
+    <h1 style="background-color: aqua; color: red">You can see the tiny houses!</h1>
+    <hr/>
     <h1> Watch the city from Twin Peaks</h1>
     <hr/>
     <em>Hint</em>: This is a RESTful web service! Append a username
